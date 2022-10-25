d0cbea91ba6c8e7661c725cf37be38647c785fe9
ChristianBaculima <christianbaculima@MacBook-Pro-de-Christian.local>
2022-10-25 08:44:29 -0500

    primer commit

---
+++ main.py
@@ -0,0 +1,31 @@
+import sys
+
+ejemplo='hola mundo esto es otra prueba'
+print(ejemplo)
+print(type(ejemplo))
+
+numero1=2
+numeor2=7
+print(numero1+numeor2)
+
+def funcion_ejemplo(x,y):
+    print(x+y)
+
+funcion_ejemplo(1,6.8)
+
+print(ejemplo.capitalize())
+
+compras = ['agua','aceite','sal','azucar','papas']
+
+for i, datos in enumerate(compras):
+    print(i, datos)
+print(0b1101)
+print(sys.float_info.min)
+print(sys.float_info.max)
+
+#valor1=input('Introdusca el primer valor')
+#valor2=input('Introdusca el segundo valor')
+#print(valor1)
+#print(valor2)
+
+print(len(ejemplo))
